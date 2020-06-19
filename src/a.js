c441fb50000c2d42839191af7ea21c22495b70bf
ryokryok <mrozisan@gmail.com>
2020-06-20 02:03:30 +0900

    live reload with dev server

--- src/a.js
+++ src/a.js
@@ -1,1 +1,4 @@
-export default () => console.log("function called");
+const endPoint = document.getElementById("root");
+export default function a() {
+  endPoint.innerHTML = "JavaScript is injected!";
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,15 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, "output"),
+    path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public/index.html"),
+    }),
+  ],
 };
